d972abcbeffc141b4e5b7d4103609a3cba2a489c
GabuGabgab <gabrielkkariara@gmail.com>
2023-05-22 10:12:46 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+    // Print the message "Programming is like building a multilingual puzzle" to the console
+    puts("\"Programming is like building a multilingual puzzle");
+
+    // Return 0 to indicate successful execution of the program
+    return 0;
 }
+
